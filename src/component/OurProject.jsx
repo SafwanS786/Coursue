21ee54f3dec1ef07c8b705e7d7e011e94cfea105
SafwanS786 <sheikhsafwan313@gmail.com>
2025-08-11 17:07:59 +0530

    ourProjectworking

--- src/component/OurProject.jsx
+++ src/component/OurProject.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../Styles/OurProject.css";
 
 export default function OurProject() {
   return (
     <div className="main_our">
-      <p id="para_our">Our Projects</p>
-      <h1 id="head_our">Projects We've Delivered</h1>
+      <div className="content_p">
+        <p id="para_our">Our Projects</p>
+        <h1 id="head_our">Projects We've Delivered</h1>
+      </div>
     </div>
   );
 }
