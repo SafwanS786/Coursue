fa1a1ba4e0362d08197c2fd398d42fa09e269256
Shafwan-web <your-email@example.com>
2025-08-27 12:19:21 +0530

    Choose_us

--- src/component/About_component/Choose.jsx
+++ src/component/About_component/Choose.jsx
@@ -4,6 +4,9 @@ import "../../Styles/About_Style/Choose.css";
 import chs from "../About_component/img/chs.jpg";
 import ch4 from "../About_component/img/ch4.jpg";
 import ch2 from "../About_component/img/ch2.jpg";
+import chs2 from "../About_component/img/chs2.avif";
+import chs3 from "../About_component/img/chs3.jpg";
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -30,7 +33,7 @@ export default function Choose() {
       } else if (window.innerWidth < 768) {
         setSettings((prevSettings) => ({ ...prevSettings, slidesToShow: 2 }));
       } else if (window.innerWidth < 1024) {
-        setSettings((prevSettings) => ({ ...prevSettings, slidesToShow: 3 }));
+        setSettings((prevSettings) => ({ ...prevSettings, slidesToShow: 2 }));
       } else {
         setSettings((prevSettings) => ({ ...prevSettings, slidesToShow: 4 }));
       }
@@ -41,7 +44,7 @@ export default function Choose() {
   }, []);
   const points = [
     "Experienced & Professional Team",
-    "Quality Work with Modern Technology",
+    "Quality Work with Modern Tech.",
     "On-Time Project Delivery",
     "Transparent & Fair Pricing",
     "24/7 Customer Support",
@@ -54,38 +57,53 @@ export default function Choose() {
       icon: ch4,
     },
     {
+      icon: chs3,
+    },
+    {
       icon: ch2,
+    },
+    {
+      icon: chs2,
     },
   ];
 
   return (
-    <section className="why-choose-us">
-      <div className="Main_Part_Chs">
-        <div className="container">
-          <h2 className="section-title">Why Choose Us</h2>
-          <p className="section-subtitle">
-            We stand apart by delivering excellence in every project.
-          </p>
+    <div className="chs_main">
+      <section className="why-choose-us">
+        <div className="Main_Part_Chs">
+          <div className="container">
+            <h2 className="section-title">Why Choose Us</h2>
+            <h1 className="section-subtitle">
+              We stand apart by delivering excellence in every project.
+            </h1>
 
-          <div className="points-grid">
-            {points.map((item, index) => (
-              <div className="point-card" key={index}>
-                <CheckCircle className="icon" />
-                <p>{item}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        {showSlider && (
-          <Slider {...setting}>
-            <div className="container down_Part">
-              {pht.map((item_pht, index_pht) => (
-                <img key={index_pht} src={item_pht.icon} alt="Choose_us" />
+            <div className="points-grid">
+              {points.map((item, index) => (
+                <div className="point-card" key={index}>
+                  <CheckCircle className="icon" />
+                  <p>{item}</p>
+                </div>
               ))}
             </div>
-          </Slider>
-        )}
-      </div>
-    </section>
+            {/* <div className="clt">
+              <p>
+                We stand out by delivering more than just exceptional results -
+                we offer a partnership built on trust, reliability and a shared
+                commitment to success. Here’s why clients continue to choose us:{" "}
+              </p>
+            </div> */}
+          </div>
+          {showSlider && (
+            <Slider {...setting}>
+              {pht.map((item_pht, index_pht) => (
+                <div className="container down_Part" key={index_pht}>
+                  <img src={item_pht.icon} alt="Choose_us" />
+                </div>
+              ))}
+            </Slider>
+          )}
+        </div>
+      </section>
+    </div>
   );
 }
